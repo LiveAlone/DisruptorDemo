acc7f826b2c313c6e96c4bb4240807ee509cac9d
LiveAlone <yaoqijunmail@gmail.com>
2017-10-31 23:20:43 +0800

    add all

--- src/main/java/org/yqj/disruptor/demo/docs/readingtest/MainTest.java
+++ src/main/java/org/yqj/disruptor/demo/docs/readingtest/MainTest.java
@@ -5,4 +5,8 @@ package org.yqj.disruptor.demo.docs.readingtest;
  */
 public class MainTest {
 
+    public static void main(String[] args) {
+        System.out.println("test condition");
+    }
+
 }
