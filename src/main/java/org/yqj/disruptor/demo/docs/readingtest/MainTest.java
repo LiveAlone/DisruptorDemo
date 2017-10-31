fc10fcbcb6cca322e2a847033fd8b77fb0f8f584
LiveAlone <yaoqijunmail@gmail.com>
2017-11-01 00:56:48 +0800

    add ll

--- src/main/java/org/yqj/disruptor/demo/docs/readingtest/MainTest.java
+++ src/main/java/org/yqj/disruptor/demo/docs/readingtest/MainTest.java
@@ -1,12 +1,7 @@
 package org.yqj.disruptor.demo.docs.readingtest;
 
-/**
- * @author yaoqijun on 2017-10-31.
- */
 public class MainTest {
-
     public static void main(String[] args) {
-        System.out.println("test condition");
+        System.out.println("测试从");
     }
-
 }
